d2add1c7982e212c2dde669f1672a77e5fe006ad
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-20 10:02:33 +0100

    functions to get if function is lower or upper added. isAlpha updated with this change

--- ft_islowercase.c
+++ ft_islowercase.c
@@ -1,19 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_islowercase.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/18 15:36:37 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/20 10:00:21 by jre-gonz         ###   ########.fr       */
+/*   Created: 2022/01/20 09:55:22 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/01/20 09:58:26 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_islowercase(char c);
-int	ft_isuppercase(char c);
-
-int	ft_isalpha(int c)
+int	ft_islowercase(char c)
 {
-	return (ft_isuppercase(c) || ft_isuppercase(c));
+	return (c >= 'a' && c <= 'z');
 }
