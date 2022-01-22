6b754a9e5873c103987aab54a55b918d3622175a
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-22 17:28:07 +0100

    norminette of ft_strrchr.c OK

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,13 +6,13 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 14:56:33 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/20 15:26:37 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/22 16:35:16 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-size_t	ft_strlen(const char* s);
+size_t	ft_strlen(const char *s);
 
 char	*ft_strrchr(const char *s, int c)
 {
@@ -22,6 +22,6 @@ char	*ft_strrchr(const char *s, int c)
 	while (i > 0 && *(s + i) != c)
 		i--;
 	if (*(s + i) == c)
-		return ((char*) s + i);
+		return ((char *) s + i);
 	return (NULL);
 }
