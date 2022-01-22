146d69a18ea3b54b17f25a95a61bd2f5a0c75adf
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-22 11:08:17 +0100

    ready to make tests

--- ft_substr.c
+++ ft_substr.c
@@ -6,13 +6,22 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 18:42:25 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/20 20:12:57 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/22 11:07:16 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
+size_t	ft_strlen(const char* s);
+size_t ft_strlcpy(char *dst, const char *src, size_t size);
+
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
-	
+	if (s == NULL || start > ft_strlen(s))
+		return (NULL);
+	char *str = malloc(sizeof(char) * len + 1);
+	if (str == NULL)
+		return (NULL);
+	ft_strlcpy(str, s + start, len);
+	return (str);
 }
