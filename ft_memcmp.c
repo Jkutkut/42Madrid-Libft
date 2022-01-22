2bbc4a0f48cfc439605c441b75b6495f7ba2aa64
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-22 20:32:05 +0100

    norminette of ft_memcmp OK\ncode refactored to pass all test

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 18:01:16 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/22 19:23:57 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/22 20:28:55 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,11 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	mem1 = (unsigned char *)s1;
 	mem2 = (unsigned char *)s2;
-	while (i < n && mem1[i] && mem2[i])
+	while (n--)
 	{
 		if (mem1[i] != mem2[i])
-			break ;
+			return (mem1[i] - mem2[i]);
 		i++;
 	}
-	if (i == n)
-		return (0);
-	return ((int) mem1[i] - (int) mem2[i]);
+	return (0);
 }
