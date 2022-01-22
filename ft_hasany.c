7beb5c28bfa0a0740b7e946cc616aecea5b4d79a
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-22 10:59:08 +0100

    has any no longer static :S

--- ft_hasany.c
+++ ft_hasany.c
@@ -6,13 +6,13 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 18:10:01 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/20 18:12:10 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/22 10:56:42 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-static int	ft_hasany(const char *str, char c)
+int	ft_hasany(const char *str, char c)
 {
 	size_t	i;
 
