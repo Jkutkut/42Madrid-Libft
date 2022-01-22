5e54a3226be05d3f20c565c8586f84482d77d9ea
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-22 17:28:07 +0100

    norminette of ft_memset.c OK

--- ft_memset.c
+++ ft_memset.c
@@ -3,18 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkutkut <jre-gonz@student.42madrid.com>    +#+  +:+       +#+        */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 19:01:01 by jkutkut           #+#    #+#             */
-/*   Updated: 2022/01/18 09:52:20 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/22 16:26:52 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-void	*ft_memset(void *s, int c, size_t n) {
-	unsigned char*	p;
-	
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*p;
+
 	p = s;
 	while (n-- > 0)
 		*p++ = (char) c;
