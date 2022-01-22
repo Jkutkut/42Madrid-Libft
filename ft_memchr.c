881e81b265167e5fc8b71b3446c29d9cc6fdf509
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-22 20:29:39 +0100

    norminette of ft_memchr.c OK
    refactored

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 16:19:52 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/22 20:17:55 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/22 20:29:09 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,11 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t			i;
 	unsigned char	*str;
 	unsigned char	ucc;
 
 	if (n == 0)
 		return (NULL);
-	i = 0;
 	str = (unsigned char *) s;
 	ucc = (unsigned char) c;
 	while (n--)
