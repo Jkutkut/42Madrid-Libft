8d9851d4b73403510e98dbc034369784eabcfaff
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-22 17:25:52 +0100

    test norminette ready

--- tests/test_norminette.sh
+++ tests/test_norminette.sh
@@ -1,5 +1,41 @@
 #!/bin/sh
 
-for f in $(ls *.c); do
-	norminette $f || break
+#colors:
+NC='\033[0m' # No Color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+LRED='\033[1;31m'
+LGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LBLUE='\033[1;34m'
+TITLE='\033[38;5;33m'
+
+tmpFile=norminetteTmp
+
+echo "${TITLE}
+  _   _                      _            _   _
+ | \ | | ___  _ __ _ __ ___ (_)_ __   ___| |_| |_ ___ 
+ |  \| |/ _ \| '__| '_ \` _ \| | '_ \ / _ \ __| __/ _ \\
+ | |\  | (_) | |  | | | | | | | | | |  __/ |_| ||  __/
+ |_| \_|\___/|_|  |_| |_| |_|_|_| |_|\___|\__|\__\___|\n\n${NC}"
+
+for f in $(ls *.h *.c); do
+	if [ ! -f "$f" ]; then
+		echo "${LRED}$f${NC} is not a file"
+		break;
+	fi
+
+	if expr "$f" : '.*\.c$' > /dev/null; then
+		norminette $f > $tmpFile
+	else
+		norminette -R CheckForbiddenSourceHeader $f > $tmpFile
+	fi &&
+	echo "$f:\t${LGREEN}OK!${NC}" ||
+	{
+		echo "$f: ${LRED}Error!${NC}"
+		sed -n '2,$p' $tmpFile
+		break
+	}
 done
+
+rm -f $tmpFile
