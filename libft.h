f067d2123ccf2317e5b9007fb4f65acab6cda21a
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-23 21:51:49 +0100

    function to get number of digits of number added

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 18:13:41 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/23 12:50:27 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/23 21:50:47 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 int		ft_islower(int c);
 int		ft_isupper(int c);
 void	ft_freearr(char **arr);
+size_t	ft_ndigits(int n);
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -49,7 +50,9 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 
 char	**ft_split(char const *s, char c);
+
 char	*ft_itoa(int n);
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
