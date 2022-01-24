745336ce36ac0930f248b3b93058a73e4cdf1d8b
Jkutkut <jre-gonz@student.42madrid.com>
2022-01-24 15:13:45 +0100

    reference to ndigits fixed

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,14 +6,14 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 19:57:46 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/23 21:52:29 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/24 15:00:06 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h> // TODO DEBUG
 #include <stdlib.h>
 
-static size_t	ft_ndigits(int n);
+size_t	ft_ndigits(int n);
 
 char	*ft_itoa(int n)
 {
