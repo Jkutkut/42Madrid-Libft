3766421a31b281ebeaecd506a0c9cdb5944f5ab0
Jkutkut <jre-gonz@student.42madrid.com>
2022-01-24 15:22:46 +0100

    islower and isupper now using int as argument (not char :S)

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,12 +6,12 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 15:36:37 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/22 19:30:42 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/24 15:21:42 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_islower(char c);
-int	ft_isupper(char c);
+int	ft_islower(int c);
+int	ft_isupper(int c);
 
 int	ft_isalpha(int c)
 {
