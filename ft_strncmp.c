6fc85f3429a5cf1c5e49e00206f0aff7e533c7ff
Jkutkut <jre-gonz@student.42madrid.com>
2022-01-24 15:38:04 +0100

    cast of result added to pass all tests

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 11:30:28 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/20 15:37:42 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/24 15:36:40 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,5 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	}
 	if (i == n)
 		return (0);
-	return (s1[i] - s2[i]);
+	return ((unsigned char) s1[i] - s2[i]);
 }
