2117cda84b7a6ae76e9d8c9968b451e3fed34ba3
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-26 10:47:44 +0100

    norminete of ft_putnbr_fd.c OK

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,18 +6,18 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 09:04:13 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/25 18:21:54 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/26 10:44:32 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
 char	*ft_itoa(int n);
-void ft_putstr_fd(char *s, int fd);
+void	ft_putstr_fd(char *s, int fd);
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char *nb;
+	char	*nb;
 
 	nb = ft_itoa(n);
 	ft_putstr_fd(nb, fd);
