c38465ca628258a8312b438e6e95082dce6dcbe3
Jkutkut <jre-gonz@student.42madrid.com>
2022-01-26 17:39:38 +0100

    split tested and norminette OK

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 12:20:17 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/26 17:06:10 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/26 17:38:27 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,12 +45,10 @@ static size_t	ft_countelements(char const *s, char c)
 	return (count);
 }
 
-char	**ft_split(char const *s, char c)
+static char	**init_split(char const *s, char c)
 {
 	char	**arr;
 	size_t	len;
-	size_t	i;
-	size_t	start;
 
 	if (s == NULL)
 		return (NULL);
@@ -59,7 +57,20 @@ char	**ft_split(char const *s, char c)
 	if (arr == NULL)
 		return (NULL);
 	arr[len] = NULL;
-	len = 0;
+	return (arr);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**arr;
+	size_t	index;
+	size_t	i;
+	size_t	start;
+
+	arr = init_split(s, c);
+	if (arr == NULL)
+		return (NULL);
+	index = 0;
 	i = 0;
 	start = 0;
 	while (s[i])
@@ -71,8 +82,8 @@ char	**ft_split(char const *s, char c)
 		start = i;
 		while (s[i] && s[i] != c)
 			i++;
-		arr[len++] = ft_substr(s, start, i - start);
-		if (arr[len - 1] == NULL)
+		arr[index++] = ft_substr(s, start, i - start);
+		if (arr[index - 1] == NULL)
 			return (ft_freesplit(arr));
 	}
 	return (arr);
