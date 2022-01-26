c09f4d078b9bcd4bb10f21ceceb2a36101a575b2
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-26 10:36:12 +0100

    norminette OK

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,15 +6,14 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 13:08:15 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/26 10:34:08 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/26 10:35:56 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
 size_t	ft_strlen(const char *s);
-int	ft_hasany(const char *str, char c);
-// int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_hasany(const char *str, char c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 char	*ft_strtrim(char const *s1, char const *set)
