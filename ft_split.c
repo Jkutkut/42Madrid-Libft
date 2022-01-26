4b492ee58e3e11e4be53fdda5aba1e61153b2f79
Jkutkut <jre-gonz@student.42madrid.com>
2022-01-26 17:16:37 +0100

    lines and done

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 12:20:17 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/26 16:11:29 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/26 17:06:10 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ static size_t	ft_countelements(char const *s, char c)
 	size_t	i;
 	size_t	count;
 
+	if (s == NULL)
+		return (0);
 	i = 0;
 	count = 0;
 	while (s[i])
@@ -35,12 +37,11 @@ static size_t	ft_countelements(char const *s, char c)
 		while (s[i] && s[i] == c)
 			i++;
 		if (!s[i])
-			break;
+			break ;
 		count++;
 		while (s[i] && s[i] != c)
 			i++;
 	}
-
 	return (count);
 }
 
@@ -48,31 +49,30 @@ char	**ft_split(char const *s, char c)
 {
 	char	**arr;
 	size_t	len;
-	size_t	index;
 	size_t	i;
 	size_t	start;
 
 	if (s == NULL)
 		return (NULL);
 	len = ft_countelements(s, c);
-	arr = malloc(sizeof(char*) * (len + 1));
+	arr = malloc(sizeof(char *) * (len + 1));
 	if (arr == NULL)
 		return (NULL);
 	arr[len] = NULL;
-	index = 0;
+	len = 0;
 	i = 0;
 	start = 0;
-	while (index < len && s[i])
+	while (s[i])
 	{
 		while (s[i] == c)
 			i++;
 		if (!s[i])
-			break;
+			break ;
 		start = i;
 		while (s[i] && s[i] != c)
 			i++;
-		arr[index++] = ft_substr(s, start, i - start);
-		if (arr[index - 1] == NULL)
+		arr[len++] = ft_substr(s, start, i - start);
+		if (arr[len - 1] == NULL)
 			return (ft_freesplit(arr));
 	}
 	return (arr);
