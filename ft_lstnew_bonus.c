58ea25dcd982e8c297adf0a37db3d6588c23a650
Jkutkut <jre-gonz@student.42madrid.com>
2022-01-26 18:59:30 +0100

    implemented. Not tested

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 17:59:52 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/26 18:58:38 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/26 18:59:14 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 t_list	*ft_lstnew(void *content)
 {
+	t_list	*e;
 
+	e = malloc(sizeof(t_list));
+	if (e == NULL)
+		return (NULL);
+	e->content = content;
+	e->next = NULL;
+	return (content);
 }
