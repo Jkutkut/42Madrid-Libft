68561abd19798ac99c3cb46053070ec095b0ea13
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-27 21:18:15 +0100

    lstclear should work now

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:03:47 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/27 12:56:58 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/27 21:16:54 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,9 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	t_list	*e;
 	if (lst == NULL || *lst == NULL)
 		return ;
-	e = *lst;
-	if (e->next != NULL)
-		ft_lstclear(&(e->next), del);
-	if (e->content != NULL)
-		ft_lstdelone(e, del);
-	free(*lst);
+	ft_lstclear(&(*lst)->next, del);
+	ft_lstdelone(*lst, del);
 	*lst = NULL;
 }
