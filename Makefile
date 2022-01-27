6d63184aa0b6ba5e6408be704cd8e5cd4d835b7b
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-27 11:23:38 +0100

    ft_lstadd_back implemented

--- Makefile
+++ Makefile
@@ -54,7 +54,7 @@ BONUS =		bin/ft_lstnew_bonus.o \
 			bin/ft_lstadd_front_bonus.o \
 			bin/ft_lstsize_bonus.o \
 			bin/ft_lstlast_bonus.o \
-			# bin/ft_lstadd_back_bonus.o \
+			bin/ft_lstadd_back_bonus.o \
 			# bin/ft_lstdelone_bonus.o \
 			# bin/ft_lstclear_bonus.o \
 			# bin/ft_lstiter_bonus.o \
--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jre-gonz <jre-gonz@student.42madrid>       +#+  +:+       +#+        */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:02:58 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/26 18:03:31 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/27 11:20:11 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-
+	if (!lst || !new || *lst == NULL)
+		return ;
+	if (*lst != NULL)
+		ft_lstadd_back(&((*lst)->next), new);
+	(*lst)->next = new;
 }
