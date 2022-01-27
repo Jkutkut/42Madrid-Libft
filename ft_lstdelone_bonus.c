585ea6f4d6ac575703ae500e92457d76126c48a8
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-27 21:11:55 +0100

    this may be ready

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:03:33 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/27 12:35:46 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/27 21:11:19 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	if (lst == NULL)
-		return;
-	del(lst->content);
-	free(lst->content);
+	if (lst == NULL || del == NULL)
+		return ;
+	if (lst->content != NULL)
+		del(lst->content);
+	free(lst);
 }
 
