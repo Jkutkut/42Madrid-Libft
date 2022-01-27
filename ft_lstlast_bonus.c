991d1a69f18813fedfe68de95c4ad4e33aa78234
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-27 11:17:41 +0100

    lst->next == NULL

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:02:24 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/26 19:56:47 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/27 11:17:19 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstlast(t_list *lst)
 {
 	if (lst == NULL)
 		return (NULL);
-	if (lst->NULL)
+	if (lst->next == NULL)
 		return (lst);
 	return (ft_lstlast(lst->next));
 }
