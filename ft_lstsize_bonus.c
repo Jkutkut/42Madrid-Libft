124b204a161e3f33f14af9ecd89b129fdb147969
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-27 18:36:58 +0100

    ft_lstsize_bonus.c tested

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -6,15 +6,18 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:01:41 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/26 19:04:49 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/27 18:35:37 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h> // TODO DEBUG
 #include "libft.h"
 
 int		ft_lstsize(t_list *lst)
 {
-	if (lst == NULL || lst->next == NULL)
+	if (lst == NULL)
 		return (0);
+	if (lst->next == NULL)
+		return (1);
 	return (1 + ft_lstsize(lst->next));
 }
---
+++ tests/test_ft_lstsize_bonus.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_ft_lstsize_bonus.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/27 17:27:54 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/01/27 18:21:12 by jre-gonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h> // TODO DEBUG
+
+void	print_list(t_list *list)
+{
+	printf("List:\n");
+	while (list != NULL)
+	{
+		if (list->content != NULL)
+			printf("%s\n", (char *)list->content);
+		else
+			printf("NULL\n");
+		list = list->next;
+	}
+}
+
+
+int	main(void)
+{
+	t_list	*e;
+
+	printf("%i\n", ft_lstsize(NULL));
+
+	e = ft_lstnew("caracola");
+	
+	printf("%i\n", ft_lstsize(e));
+	
+	ft_lstadd_front(&e, ft_lstnew("Hola"));
+
+	printf("%i\n", ft_lstsize(e));
+
+	print_list(e);
+
+	return (0);
+}
