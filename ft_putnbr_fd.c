a47d5668e8f503c17695b0074de41893d9775db0
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-27 21:01:03 +0100

    putnbr_fd no longer using free. :S

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,20 +6,28 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 09:04:13 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/26 10:44:32 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/27 21:00:11 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_itoa(int n);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putchar_fd(char s, int fd);
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*nb;
-
-	nb = ft_itoa(n);
-	ft_putstr_fd(nb, fd);
-	free(nb);
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+	}
+	if (n >= 10)
+		ft_putnbr_fd(n / 10, fd);
+	ft_putchar_fd(n % 10 + '0', fd);
 }
