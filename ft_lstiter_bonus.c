23e2f5dcda0b60c8b20a0bde2d9aebeebaa3c72e
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-27 13:55:19 +0100

    ft_lstiter_bonus.c implemented

--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:03:54 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/26 18:36:57 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/27 13:01:04 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	
+	if (lst == NULL)
+		return ;
+	if (lst->content != NULL)
+		f(lst->content);
+	if (lst->next != NULL)
+		ft_lstiter(lst->next, f);
 }
