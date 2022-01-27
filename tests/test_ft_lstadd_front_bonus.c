7ce277a5884bb02d2ae881e15800f8981cd16df6
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-27 18:15:54 +0100

    ft_lstadd_front tests made and passed

--- tests/test_ft_lstadd_front_bonus.c
+++ tests/test_ft_lstadd_front_bonus.c
@@ -1,19 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
+/*   test_ft_lstadd_front_bonus.c                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/26 18:00:55 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/27 18:07:53 by jre-gonz         ###   ########.fr       */
+/*   Created: 2022/01/27 17:27:54 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/01/27 18:12:59 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h> // TODO DEBUG
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void	print_list(t_list *list)
 {
-	new->next = *lst;
-	*lst = new;
+	printf("List:\n");
+	while (list != NULL)
+	{
+		if (list->content != NULL)
+			printf("%s\n", (char *)list->content);
+		else
+			printf("NULL\n");
+		list = list->next;
+	}
+}
+
+
+int	main(void)
+{
+	t_list	*e;
+
+	e = ft_lstnew("caracola");
+	
+	ft_lstadd_front(&e, ft_lstnew("Hola"));
+
+	print_list(e);
+
+	return (0);
 }
