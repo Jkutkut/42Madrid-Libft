d58115dacb54aa481782bc521d06b4e468838335
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-27 22:04:51 +0100

    ft_lstsize_bonus.c norminette OK

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -6,14 +6,13 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:01:41 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/27 18:35:37 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/27 22:02:22 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h> // TODO DEBUG
 #include "libft.h"
 
-int		ft_lstsize(t_list *lst)
+int	ft_lstsize(t_list *lst)
 {
 	if (lst == NULL)
 		return (0);
