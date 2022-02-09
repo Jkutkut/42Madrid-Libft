0ed02d42c13a0270d578aa88e48657690ce79997
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-09 12:20:50 +0100

    hasany proto added back

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 16:53:34 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/02/09 12:16:13 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/09 12:20:27 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 #define SPACES " \n\v\f\r\t"
 #define NUMBERS "0123456789"
+
+int	ft_hasany(const char *str, char c);
 
 int	ft_atoi(char *str)
 {
