29405682987181d708a00240f019e642a96bce6c
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-09 12:22:04 +0100

    now usign prototypes

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,19 +6,13 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 15:36:37 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/27 23:07:21 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/09 12:21:45 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static int	ft_islower(int c)
-{
-	return (c >= 'a' && c <= 'z');
-}
+int	ft_islower(int c);
 
-static int	ft_isupper(int c)
-{
-	return (c >= 'A' && c <= 'Z');
-}
+int	ft_isupper(int c);
 
 int	ft_isalpha(int c)
 {
