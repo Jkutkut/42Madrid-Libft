8c7d25cabe60b8c52d3b7f54c37766e61a18b4b1
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-09 12:30:44 +0100

    tolower now using isupper

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,14 +6,11 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 16:24:56 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/27 23:07:49 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/09 12:30:19 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static int	ft_isupper(int c)
-{
-	return (c >= 'A' && c <= 'Z');
-}
+int	ft_isupper(int c);
 
 int	ft_tolower(int c)
 {
