b5757bce3e4dcbfc57890de08c4555d15df90e88
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-07 13:28:07 +0100

    updated

--- keepOnlyCode.sh
+++ keepOnlyCode.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 dirsToRemove=".git .info .tests"
+filesToRemove=".gitignore"
 
+echo "Removing directories:\n$dirsToRemove"
 rm -rf $dirsToRemove
+
+echo "Removing files"
+rm $filesToRemove
+
+echo "Removing script"
+rm $0
