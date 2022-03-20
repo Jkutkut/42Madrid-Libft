70d11c2d5f679e7030f982919a8816f7351e2f83
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:52 +0100

    ft_ndigits.c documented

--- ft_ndigits.c
+++ ft_ndigits.c
@@ -6,12 +6,18 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 12:25:53 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/07 15:08:17 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:47:24 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
+/**
+ * @brief Returns the number of digits needed to represent a number.
+ * 
+ * @param n Number to represent.
+ * @return size_t Number of digits to represent it.
+ */
 size_t	ft_ndigits(int n)
 {
 	size_t	order;
