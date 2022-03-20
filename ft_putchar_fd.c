29ff3873b7f2f504ad2b8518a50672991d498732
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:52 +0100

    ft_putchar_fd.c documented

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,12 +6,18 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 08:54:24 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/26 10:44:38 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:46:58 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+/**
+ * @brief Writes a character to a file descriptor.
+ * 
+ * @param c Character to write.
+ * @param fd File descriptor.
+ */
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
