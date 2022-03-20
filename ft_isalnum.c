0565fa471c9b9537bc050f5e275460121c3fa642
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:51 +0100

    ft_isalnum.c documented

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,13 +6,19 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 17:53:42 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/27 23:35:47 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 16:00:12 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 
+/**
+ * @brief Checks if a character is alphanumeric.
+ * 
+ * @param c Character to check.
+ * @return int 0 if it is not alphanumeric, true otherwise.
+ */
 int	ft_isalnum(int c)
 {
 	return (ft_isalpha(c) || ft_isdigit(c));
