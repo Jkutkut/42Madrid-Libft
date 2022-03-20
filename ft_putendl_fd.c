8233e37c1e95db6c7c3b07fdb5ba980e2392b5ad
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:52 +0100

    ft_putendl_fd.c documented

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,18 +6,25 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 09:00:36 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/26 10:44:35 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:46:46 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putchar_fd(char c, int fd);
 
+/**
+ * @brief Prints the string s to the file descriptor fd with '\n' at the end.
+ * 
+ * @param s String to print.
+ * @param fd File descriptor.
+ */
 void	ft_putendl_fd(char *s, int fd)
 {
 	if (s == NULL)
 		return ;
 	ft_putstr_fd(s, fd);
-	ft_putstr_fd("\n", fd);
+	ft_putchar_fd('\n', fd);
 }
