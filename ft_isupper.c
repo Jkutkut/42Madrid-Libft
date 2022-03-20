c8cdfcd05354fa4fae29aab9f676423afe42c507
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:51 +0100

    ft_isupper.c documented

--- ft_isupper.c
+++ ft_isupper.c
@@ -6,10 +6,16 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 12:21:37 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/07 15:08:34 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:58:37 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @brief Checks if the given character is uppercase.
+ * 
+ * @param c Character to check.
+ * @return int 0 if it is not uppercase, true otherwise.
+ */
 int	ft_isupper(int c)
 {
 	return (c >= 'A' && c <= 'Z');
