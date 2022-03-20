50f34f02a7ebb2fcd5c34c093fbdfc347b9f213b
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:51 +0100

    ft_isascii.c documented

--- ft_isascii.c
+++ ft_isascii.c
@@ -3,13 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jre-gonz <jre-gonz@student.42madrid>       +#+  +:+       +#+        */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 17:53:42 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/18 18:08:25 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:59:45 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @brief Checks if a character is ASCII.
+ * 
+ * @param c Character to check.
+ * @return int 0 if it is not ASCII, true otherwise.
+ */
 int	ft_isascii(int c)
 {
 	return (c >= 0 && c <= 127);
