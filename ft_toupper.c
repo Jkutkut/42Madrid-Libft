2c04c2335ab51102a768e6a266b597cd5be5ef92
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 13:48:08 +0100

    ft_toupper.c documented

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,12 +6,18 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 16:29:11 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/02/09 12:32:22 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 13:38:22 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_islower(int c);
 
+/**
+ * @brief Upper-cases a character.
+ * 
+ * @param c Character to convert.
+ * @return int Converted character.
+ */
 int	ft_toupper(int c)
 {
 	if (ft_islower(c))
