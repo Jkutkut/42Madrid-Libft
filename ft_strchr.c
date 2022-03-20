fcf0e48f2804409093f2f9cf45dee29af1c41683
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:52 +0100

    ft_strchr.c documented

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,12 +6,20 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 14:56:33 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/22 20:38:49 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:36:37 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
+/**
+ * @brief Returns a pointer to the first occurrence of the character c in the
+ * string s.
+ * 
+ * @param s String to search.
+ * @param c Character to search for.
+ * @return char* Pointer to the occurrence or NULL if not found.
+ */
 char	*ft_strchr(const char *s, int c)
 {
 	char	cc;
