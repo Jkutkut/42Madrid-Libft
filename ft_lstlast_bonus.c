731cc7eaf5ba44ef6a583236abb19b5d15e18497
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:52 +0100

    ft_lstlast_bonus.c documented

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -6,12 +6,18 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:02:24 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/27 19:49:34 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:54:32 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @brief Returns the last element of a linked list.
+ * 
+ * @param lst Linked list.
+ * @return t_list* Pointer to the last element.
+ */
 t_list	*ft_lstlast(t_list *lst)
 {
 	if (lst == NULL)
