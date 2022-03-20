5559f7daec1a95d1614e59fbefdd7ee53f65d526
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 13:48:08 +0100

    ft_tolower.c documented

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,12 +6,18 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 16:24:56 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/02/09 12:30:19 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 13:38:39 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isupper(int c);
 
+/**
+ * @brief Converts a character to lowercase.
+ * 
+ * @param c Character to convert.
+ * @return int Lowercase character.
+ */
 int	ft_tolower(int c)
 {
 	if (ft_isupper(c))
