855d1a083aff21596348bfe3153004b27e37e1e0
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 13:48:08 +0100

    ft_strncmp.c documented

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,12 +6,23 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 11:30:28 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/24 15:36:40 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 13:44:30 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
+/**
+ * @brief Compares the first n characters of the strings s1 and s2.
+ * If the strings are equal, 0 is returned.
+ * If they are different, the difference between the first different char is
+ * returned.
+ * 
+ * @param s1
+ * @param s2
+ * @param n Maximum number of characters to compare.
+ * @return int Returns the comparison result.
+ */
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
