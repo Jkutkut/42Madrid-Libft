133a4ac664269479fe6be003b41c504cbb2223ad
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 13:48:08 +0100

    ft_strlen.c documented

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,12 +6,18 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 19:01:01 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/27 23:01:04 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 13:45:52 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
+/**
+ * @brief Returns the length of the string s.
+ * 
+ * @param s String to get the length.
+ * @return size_t Length of the string s.
+ */
 size_t	ft_strlen(const char *s)
 {
 	size_t	l;
