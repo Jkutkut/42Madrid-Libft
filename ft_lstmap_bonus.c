79b687a9239d6db7504b66c1184b06b76c87e6cb
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:52 +0100

    ft_lstmap_bonus.c documented

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:04:01 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/27 22:01:59 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:54:17 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,16 @@ t_list	*ft_lstnew(void *content);
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstadd_back(t_list **lst, t_list *new);
 
+/**
+ * @brief Creates a new linked list with the result of applying f to each
+ * element.
+ * It uses del function to free the memory of the elements of the original.
+ * 
+ * @param lst Linked list.
+ * @param f Function to apply to each element.
+ * @param del Function to free the memory of the elements of the original.
+ * @return t_list* Pointer to the new linked list.
+ */
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
