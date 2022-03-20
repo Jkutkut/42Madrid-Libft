3a831f531e973b54bc75a268598d1864f14d11fa
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:51 +0100

    ft_bzero.c documented

--- ft_bzero.c
+++ ft_bzero.c
@@ -12,6 +12,13 @@
 
 #include <stddef.h>
 
+/**
+ * @brief Empties the given memory area.
+ * 
+ * @param s Memory area.
+ * @param n Number of bytes to empty.
+ * @return void* Pointer to the memory area (same as input).
+ */
 void	*ft_bzero(void *s, size_t n)
 {
 	unsigned char	*p;
