cc7c7b4fd69b048fffaf40f8d139d398abfb2540
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:51 +0100

    ft_islower.c documented

--- ft_islower.c
+++ ft_islower.c
@@ -6,10 +6,16 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 12:19:46 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/07 15:08:02 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:59:04 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @brief Checks if the given character is lowercase.
+ * 
+ * @param c Character to check.
+ * @return int 0 if it is not lowercase, true otherwise.
+ */
 int	ft_islower(int c)
 {
 	return (c >= 'a' && c <= 'z');
