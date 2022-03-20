9d8a7fd62efc5b8883132e6e348b8d2124567fbd
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:51 +0100

    ft_lstadd_front_bonus.c documented

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,12 +6,18 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:00:55 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/27 18:07:53 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:57:20 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @brief Adds a new element at the beginning of a linked list.
+ * 
+ * @param lst Linked list.
+ * @param new New element to add.
+ */
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	new->next = *lst;
