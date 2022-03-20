eafc44b0a6646c025468ab69ce2c7c926bf34c6c
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:51 +0100

    ft_isdigit.c documented

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -3,13 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jre-gonz <jre-gonz@student.42madrid>       +#+  +:+       +#+        */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 15:36:37 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/18 17:52:29 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:59:33 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @brief Checks if a character is a digit.
+ * 
+ * @param c Character to check.
+ * @return int 0 if it is not a digit, true otherwise.
+ */
 int	ft_isdigit(int c)
 {
 	return (c >= '0' && c <= '9');
