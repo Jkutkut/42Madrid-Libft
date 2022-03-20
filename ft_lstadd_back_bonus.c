9548c06630fc21780e30f9f34a43cfbfccb86ebf
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:51 +0100

    ft_lstadd_back_bonus.c documented

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,12 +6,18 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:02:58 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/27 20:19:29 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:57:33 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @brief Adds a new element at the end of a linked list.
+ * 
+ * @param lst Linked list.
+ * @param new New element.
+ */
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	if (lst == NULL || new == NULL)
