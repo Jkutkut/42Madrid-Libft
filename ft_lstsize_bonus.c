bd903337b5f4316153ae973c750ae61f037dffa4
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:52 +0100

    ft_lstsize_bonus.c documented

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -6,12 +6,18 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:01:41 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/27 22:02:22 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:51:35 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @brief Returns the number of elements in a linked list.
+ * 
+ * @param lst Linked list.
+ * @return int Number of elements.
+ */
 int	ft_lstsize(t_list *lst)
 {
 	if (lst == NULL)
