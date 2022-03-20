5f3712ead67331b7415290ee5fa7c6fa7c3a262a
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:52 +0100

    ft_lstnew_bonus.c documented

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,12 +6,18 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 17:59:52 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/27 18:02:04 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:52:01 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @brief Creates a new element of a linked list and returns a pointer to it.
+ * 
+ * @param content Pointer to the content of the new element.
+ * @return t_list* Pointer to the new element.
+ */
 t_list	*ft_lstnew(void *content)
 {
 	t_list	*e;
