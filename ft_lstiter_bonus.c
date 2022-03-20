d8760b9ff244b1f114053d933a32c3af15368f70
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:52 +0100

    ft_lstiter_bonus.c documented

--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -6,12 +6,18 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:03:54 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/27 13:01:04 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:54:56 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @brief Iterates a linked list, running f on each element.
+ * 
+ * @param lst Linked list.
+ * @param f Function to run on each element.
+ */
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	if (lst == NULL)
