ccb19a6e6cf2801ebf81c7ee4ed695801b39e48e
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:51 +0100

    ft_isalpha.c documented

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,14 +6,19 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 15:36:37 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/02/09 12:21:45 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:59:59 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_islower(int c);
-
 int	ft_isupper(int c);
 
+/**
+ * @brief Checks if the given character is alphabetic.
+ * 
+ * @param c Character to check.
+ * @return int 0 if it is not alphabetic, true otherwise.
+ */
 int	ft_isalpha(int c)
 {
 	return (ft_isupper(c) || ft_islower(c));
