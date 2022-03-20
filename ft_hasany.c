c916f7cb6f091bda113a6f7fc6e38322a6ed914c
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:51 +0100

    ft_hasany.c documented

--- ft_hasany.c
+++ ft_hasany.c
@@ -6,12 +6,19 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 12:15:56 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/07 15:08:54 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 16:01:02 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
+/**
+ * @brief Checks if the given char c is any of the chars in the given string.
+ * 
+ * @param str String to check.
+ * @param c Character to check.
+ * @return int 0 if it is not any of the chars in the string, 1 otherwise.
+ */
 int	ft_hasany(const char *str, char c)
 {
 	size_t	i;
