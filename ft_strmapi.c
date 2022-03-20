a207dff9e43f957c5da1a9c23f556602026c34e9
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 13:48:08 +0100

    ft_strmapi.c documented

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 14:45:25 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/24 22:28:31 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 13:45:37 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 
 size_t	ft_strlen(const char *s);
 
+/**
+ * @brief Applies the function f to each character of the given string.
+ * 
+ * @param s String to apply the function to.
+ * @param f Function to apply.
+ * @return char* New string with the function applied.
+ */
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	size_t	i;
