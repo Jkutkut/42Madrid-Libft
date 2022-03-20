912d47ad675e19f1f7468c3c8c2739c367d9dd53
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:52 +0100

    ft_memset.c documented

--- ft_memset.c
+++ ft_memset.c
@@ -6,12 +6,20 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 19:01:01 by jkutkut           #+#    #+#             */
-/*   Updated: 2022/01/22 16:26:52 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:48:14 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
+/**
+ * @brief Sets the first n bytes of the memory area pointed by s to c.
+ * 
+ * @param s Pointer to the memory area.
+ * @param c Character to set.
+ * @param n Number of bytes to set.
+ * @return void* Same pointer as s.
+ */
 void	*ft_memset(void *s, int c, size_t n)
 {
 	unsigned char	*p;
