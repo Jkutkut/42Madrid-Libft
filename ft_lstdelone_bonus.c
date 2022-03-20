f270513e5ac61eb5c646d9e3758fcdfd4872cf42
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 16:05:52 +0100

    ft_lstdelone_bonus.c documented

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,12 +6,18 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:03:33 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/27 22:01:46 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:55:48 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @brief Deletes the given element with the given function.
+ * 
+ * @param lst Element to delete.
+ * @param del Function to delete the element.
+ */
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	if (lst == NULL || del == NULL)
