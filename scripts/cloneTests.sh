defe4943d80322adc81b312dacfee1e5b0057ebc
Jkutkut <jorgeregonzalez@gmail.com>
2022-05-25 09:58:13 +0200

    CloneTests script ready

--- scripts/cloneTests.sh
+++ scripts/cloneTests.sh
@@ -2,9 +2,9 @@
 
 tests="https://github.com/alelievr/libft-unit-test.git https://github.com/Tripouille/libftTester.git https://github.com/y3ll0w42/libft-war-machine.git"
 
-for i in $tests; do
-	echo "Cloning $i"
-	# git clone $t
+for t in $tests; do
+	echo "Cloning $t"
+	git clone $t
 done
 
 echo "Done"
