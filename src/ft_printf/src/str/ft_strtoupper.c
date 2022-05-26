bb060cf063b28237a53a7d605ae21bc7dd308c1a
Jkutkut <jorgeregonzalez@gmail.com>
2022-05-26 13:35:53 +0200

    removing files from db

--- src/ft_printf/src/str/ft_strtoupper.c
+++ src/ft_printf/src/str/ft_strtoupper.c
@@ -6,11 +6,11 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 16:29:11 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/02/13 22:47:17 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/05/26 12:52:40 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static int	ft_toupper(int c)
+static int	ft_touppper(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (c - 32);
@@ -21,7 +21,7 @@ void	ft_strtoupper(char *str)
 {
 	while (*str)
 	{
-		*str = ft_toupper(*str);
+		*str = ft_touppper(*str);
 		str++;
 	}
 }
