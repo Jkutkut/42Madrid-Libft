804699743faa817f9349e71ab82cc1a553f00182
Jkutkut <jorgeregonzalez@gmail.com>
2022-05-26 11:07:00 +0200

    Now able to compile GNL using libft

---
+++ src/get_next_line/get_next_line.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/28 10:00:44 by jkutkut           #+#    #+#             */
+/*   Updated: 2022/03/24 15:37:21 by jre-gonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 4096
+# endif
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4096
+# endif
+
+typedef struct s_chunk
+{
+	char			*content;
+	struct s_chunk	*next;
+}					t_chunk;
+
+char	*get_next_line(int fd);
+
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
