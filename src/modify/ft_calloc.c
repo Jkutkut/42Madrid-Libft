9b0fb3b87f0aec1c627859bcb90fc7410d7cad32
Jkutkut <jorgeregonzalez@gmail.com>
2022-10-18 17:40:11 +0200

    callor refactored

--- src/modify/ft_calloc.c
+++ src/modify/ft_calloc.c
@@ -6,11 +6,13 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 18:32:48 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/20 16:02:16 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/10/18 17:29:28 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+
+void	*ft_bzero(void *s, size_t n);
 
 /**
  * @brief Allocates (with malloc) and clears the requested amount of memory.
@@ -22,14 +24,9 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	unsigned char	*p;
-	size_t			i;
 
 	p = malloc(count * size);
 	if (p != NULL)
-	{
-		i = 0;
-		while (i < count * size)
-			*(p + i++) = 0;
-	}
+		ft_bzero(p, count * size);
 	return (p);
 }
